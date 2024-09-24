f91afec95ae80bb4ff25dfaaa23c2856aeedde86
Danila Bernat <danila.bernat@innowise.com>
2024-09-24 18:17:52 +0200

    Refactor IPFormatConverter

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -8,18 +8,20 @@
     const int minAllowedIPv4PartValue = 0;
 
     if ([numbersArray count] == 0 || numbersArray == nil) { return @""; }
+    
+    NSMutableArray *ipPartsArray = [NSMutableArray arrayWithCapacity:4];
 
-    NSMutableArray *resultIPv4NumbersArray = [[NSMutableArray alloc] init];
-    for (int i = 0; i < [numbersArray count] || i < maxIPv4AllowedParts; i++) {
-        if (i < [numbersArray count] && [numbersArray[i] intValue] < minAllowedIPv4PartValue) { return @"Negative numbers are not valid for input."; }
-        if (i < [numbersArray count] && [numbersArray[i] intValue] > maxAllowedIPv4PartValue) { return @"The numbers in the input array can be in the range from 0 to 255."; }
-
-        if (i + 1 > maxIPv4AllowedParts) { break; }
-        if (i + 1 > [numbersArray count]) { [resultIPv4NumbersArray addObject:@0]; continue; }
-        [resultIPv4NumbersArray addObject:numbersArray[i]];
+    for (int i = 0; i < [numbersArray count] && i < maxIPv4AllowedParts; i++) {
+        if ([numbersArray[i] intValue] < minAllowedIPv4PartValue) { return @"Negative numbers are not valid for input."; }
+        if ([numbersArray[i] intValue] > maxAllowedIPv4PartValue) { return @"The numbers in the input array can be in the range from 0 to 255."; }
+        ipPartsArray[i] = numbersArray[i];
     }
 
-    return [resultIPv4NumbersArray componentsJoinedByString:@"."];
+    while ([ipPartsArray count] < 4) {
+        [ipPartsArray addObject:@0];
+    }
+
+    return [ipPartsArray componentsJoinedByString:@"."];
 }
 
 @end
