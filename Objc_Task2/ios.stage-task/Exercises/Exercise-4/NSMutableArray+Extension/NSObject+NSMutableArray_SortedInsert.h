a9536ef736a351b143eb05ce9b71793878263dad
Danila Bernat <danila.bernat@innowise.com>
2024-09-24 18:56:46 +0200

    Done TwoDimensionalArraySorter

---
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/NSMutableArray+Extension/NSObject+NSMutableArray_SortedInsert.h
@@ -0,0 +1,14 @@
+//
+//  NSObject+NSMutableArray_SortedInsert.h
+//  ios.stage-task
+//
+//  Created by Danila Bernat on 24/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (SortedInsert)
+
+- (void)insertObjectInSortedOrder:(id)object;
+
+@end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -1,9 +1,34 @@
 #import "TwoDimensionalArraySorter.h"
+#import "NSMutableArray+Extension/NSObject+NSMutableArray_SortedInsert.h"
 
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    NSMutableArray *resultStrings = [NSMutableArray new];
+    NSMutableArray *resultNumbers = [NSMutableArray new];
+
+    if ([array count] > 0 && ![array[0] isKindOfClass:[NSArray class]]) { return @[]; }
+
+    Class currentSubarrayType;
+
+    for (NSArray *subArray in array) {
+        if ([subArray count] > 0) {
+            currentSubarrayType = [subArray[0] class];
+        }
+        for (id element in subArray) {
+            if ([element class] != currentSubarrayType) { return @[]; }
+
+            if ([element isKindOfClass:[NSString class]]) {
+                [resultStrings insertObjectInSortedOrder:element];
+            } else if ([element isKindOfClass:[NSNumber class]]) {
+                [resultNumbers insertObjectInSortedOrder:element];
+            }
+        }
+    }
+
+    if ([resultStrings count] > 0) { return resultStrings; } else {
+        return resultNumbers;
+    }
 }
 
 @end
