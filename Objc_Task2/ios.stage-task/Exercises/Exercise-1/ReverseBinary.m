647dc03956cca4cdd05934b8fa0a83f562e651b2
Danila Bernat <danila.bernat@innowise.com>
2024-09-24 15:45:05 +0200

    Refactor ReverseInteger

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -4,7 +4,7 @@ UInt8 ReverseInteger(UInt8 n) {
     unsigned char reversed = 0;
 
     for (int i = 0; i < 8; i++) {
-        UInt8 lastBit = n & 1;
+        bool lastBit = n & 1;
         reversed <<= 1;
         reversed |= lastBit;
         n >>= 1;
